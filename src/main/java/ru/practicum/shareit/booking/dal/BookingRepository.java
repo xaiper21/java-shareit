cdad40793c1a16ca45d250461d63dee88293fcac
Kirill <kireeffkirill21@yandex.ru>
2025-08-22 09:19:07 +0300

    codestyle

--- src/main/java/ru/practicum/shareit/booking/dal/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/dal/BookingRepository.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.booking.dal;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.shareit.booking.Booking;
 import ru.practicum.shareit.booking.BookingStatus;
 
